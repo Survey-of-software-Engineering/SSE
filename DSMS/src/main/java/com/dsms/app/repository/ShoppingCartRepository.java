6dc10db4422aac15ca269fbe8111029df6dc574b
krishnaathipatla <krishnaathipatla@gmail.com>
2023-11-29 11:55:13 +0530

    Fix

--- DSMS/src/main/java/com/dsms/app/repository/ShoppingCartRepository.java
+++ DSMS/src/main/java/com/dsms/app/repository/ShoppingCartRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ShoppingCartRepository extends MongoRepository<ShoppingCart,String> {
+public interface ShoppingCartRepository extends MongoRepository<ShoppingCart, String> {
 }
